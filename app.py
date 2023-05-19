c60df0ffe90cf090128eb58ec532a248509ff030
Devayani <devayanipawar26@gmail.com>
2023-05-19 11:16:53 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+from analysis import plot1
+
+
+app=Flask(__name__)
+
+@app.route("/")
+def visual1():
+     graph1=plot1()
+     return render_template('index.html', graph_1=graph1)
